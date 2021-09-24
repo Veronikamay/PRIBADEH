ba5aecb5743e95ccb62ba9245ae185d64a562cf1
panggil aku shifa <87982800+Veronikamay@users.noreply.github.com>
2021-09-25 01:23:01 +0700

    Add files via upload

--- plugins/delmsg.js
+++ plugins/delmsg.js
@@ -1,14 +1,13 @@
 let handler = async (m, { command, usedPrefix, text }) => {
-    let which = command.replace(/get/i, '')
-    if (!text) throw `Gunakan *${usedPrefix}list${which}* untuk melihat list nya`
-    let msgs = global.DATABASE._data.msgs
-    if (!text in msgs) throw `'${text}' tidak terdaftar di list pesan`
+    let which = command.replace(/del/i, '')
+    if (!text) throw `Gunakan *${usedPrefix}list${which}* untuk melihat daftar nya`
+    let msgs = global.db.data.msgs
+    if (!text in msgs) throw `'${text}' tidak terdaftar di daftar pesan`
     delete msgs[text]
-    m.reply(`Berhasil menghapus pesan di list pesan dengan nama '${text}'`)
+    m.reply(`Berhasil menghapus pesan di daftar pesan dengan nama '${text}'`)
 }
-handler.help = ['vn', 'msg', 'video', 'audio', 'img', 'sticker'].map(v => 'del' + v + ' <text>')
+handler.help = ['vn', 'msg', 'video', 'audio', 'img', 'sticker', 'gif'].map(v => 'del' + v + ' <teks>')
 handler.tags = ['database']
-handler.command = /^del(vn|msg|video|audio|img|sticker)$/
-handler.register = true
+handler.command = /^del(vn|msg|video|audio|img|stic?ker|gif)$/
 
 module.exports = handler
